fd5b09e6a0b743ddf15c4c3879c9bd531d72ca32
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-03-10 08:26:46 +0900

    Improve tabs performance

--- src/tabs/tab-content-list-item.js
+++ src/tabs/tab-content-list-item.js
@@ -1,14 +1,27 @@
 import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
 
 export class TabContentListItem extends Component {
   render() {
+    let isHidden = false;
+    if (
+      this.props.index < this.context.currentIndex - 1 ||
+      this.context.currentIndex + 1 < this.props.index
+    ) {
+      isHidden = true;
+    }
     return (
       <section
         onTouchStart={this.context.handleTouchStart}
         onTouchMove={this.context.handleTouchMove}
         onTouchEnd={this.context.handleTouchEnd}
-        className="tab-content-list-item"
-        >{this.props.children}</section>
+        className={classNames(
+          "tab-content-list-item",
+          {'tab-content-list-item__hidden': isHidden}
+        )}
+        >
+        <section className="tab-content-list-item-inner">{this.props.children}</section>
+      </section>
     );
   }
 }
